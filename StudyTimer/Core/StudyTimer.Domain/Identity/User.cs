d790e0bb7a25b7ee974feadc14acad313dda07fb
Bengisoo <zehrabengisu8@gmail.com>
2023-12-11 22:58:25 +0300

    [Added] Identity

---
+++ StudyTimer/Core/StudyTimer.Domain/Identity/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using StudyTimer.Domain.Common;
+using StudyTimer.Domain.Entities;
+using StudyTimer.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyTimer.Domain.Identity
+{
+    public class User : IdentityUser<Guid>, IEntityBase<Guid>, ICreatedOn, IModifiedOn
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? CreatedByUserId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public string? ModifiedByUserId { get; set; }
+        public ICollection<UserStudySession> Sessions { get; set; }
+    }
+}
