edda4be65a7b9a277dc2823513ab6cf143642ada
Bengisoo <zehrabengisu8@gmail.com>
2023-12-11 22:56:42 +0300

    [Added] MVC

---
+++ StudyTimer/Presentation/StudyTimer.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudyTimer.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
