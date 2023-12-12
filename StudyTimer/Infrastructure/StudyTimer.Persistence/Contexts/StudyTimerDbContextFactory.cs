ab3ecdf8f4eb6b934cdf35870b79a7c5adca5400
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-12 17:37:28 +0300

    [Added] : Study timer db context factory cs added

---
+++ StudyTimer/Infrastructure/StudyTimer.Persistence/Contexts/StudyTimerDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyTimer.Persistence.Contexts
+{
+    public class StudyTimerDbContextFactory : IDesignTimeDbContextFactory<StudyTimerDbContext>
+    {
+        public StudyTimerDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<StudyTimerDbContext>();
+
+            var connectionString = configuration.GetSection("Team7PostgreSQLDB").Value;
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new StudyTimerDbContext(optionsBuilder.Options);
+        }
+    }
+}
