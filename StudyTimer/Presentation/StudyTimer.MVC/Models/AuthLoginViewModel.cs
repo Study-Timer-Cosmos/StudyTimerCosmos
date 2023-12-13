09334f9ae7622bdface6878374e8c3e0b2624d09
Bengisoo <zehrabengisu8@gmail.com>
2023-12-13 21:40:20 +0300

    [Added] AuthLoginViewModel

---
+++ StudyTimer/Presentation/StudyTimer.MVC/Models/AuthLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyTimer.MVC.Models
+{
+    public class AuthLoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
