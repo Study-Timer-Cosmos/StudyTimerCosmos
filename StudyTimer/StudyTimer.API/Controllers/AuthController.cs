29d85c598e78e879fefa9be7511fcac14a23117a
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-13 04:06:26 +0300

    [Update]  StudyTimer.API auth controller

---
+++ StudyTimer/StudyTimer.API/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudyTimer.Application.Models;
+using StudyTimer.Domain.Identity;
+
+namespace StudyTimer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        //private readonly IToastNotification _toastNotification;
+        //private readonly IResend _resend;
+        private readonly IWebHostEnvironment _environment;
+
+        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager, IWebHostEnvironment environment)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _environment = environment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync([FromBody] AuthRegisterViewModel registerViewModel)
+        {
+            var userId = Guid.NewGuid();
+
+            var user = new User()
+            {
+                Id = userId,
+                Email = registerViewModel.Email,
+                FirstName = registerViewModel.FirstName,
+                LastName = registerViewModel.SurName,
+                Gender = registerViewModel.Gender,
+                BirthDate = registerViewModel.BirthDate,
+                UserName = registerViewModel.UserName,
+                Sessions = registerViewModel.Sessions,
+                CreatedOn = DateTime.UtcNow,
+                CreatedByUserId = userId.ToString()
+            };
+
+            var identityResult = await _userManager.CreateAsync(user, registerViewModel.Password);
+
+            if (identityResult.Succeeded)
+            {
+                // User creation was successful.
+                // You might want to customize the response based on your requirements.
+                return Ok(new { Message = "User registration successful." });
+            }
+            else
+            {
+                // User creation failed. Return an appropriate response.
+                return BadRequest(new { Errors = identityResult.Errors });
+            }
+        }
+
+
+    }
+}
--- StudyTimer/StudyTimer.API/Program.cs
+++ StudyTimer/StudyTimer.API/Program.cs
@@ -1,6 +1,11 @@
+using StudyTimer.Persistence.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddDbContext<StudyTimerDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
