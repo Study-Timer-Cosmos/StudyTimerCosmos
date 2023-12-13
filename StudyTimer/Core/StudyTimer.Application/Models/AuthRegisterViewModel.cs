60b24136b9c3c21aaafb8e6572c4e26cfc0ab102
Bengisoo <zehrabengisu8@gmail.com>
2023-12-13 21:34:03 +0300

    [Updated] AuthRegisterViewModel

--- StudyTimer/Core/StudyTimer.Application/Models/AuthRegisterViewModel.cs
+++ StudyTimer/Core/StudyTimer.Application/Models/AuthRegisterViewModel.cs
@@ -11,21 +11,19 @@ namespace StudyTimer.Application.Models
 {
     public class AuthRegisterViewModel
     {
-        [Required]
+        
         public string Email { get; set; }
-        [Required]
-        [MinLength(2)]
-        public string UserName { get; set; }
-        [Required]
-        [MinLength(6)]
+        
+        
+        
         public string Password { get; set; }
-        [Required]
+        
         public string FirstName { get; set; }
-        [Required]
-        public string SurName { get; set; }
+        
+        public string LastName { get; set; }
         public DateTime? BirthDate { get; set; }
-        [Required]
+        
         public Gender Gender { get; set; }
-        public ICollection<UserStudySession> Sessions { get; set; }
+        
     }
 }
