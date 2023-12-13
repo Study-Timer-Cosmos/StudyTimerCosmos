2275a1368f8e12dc598920b54e2222ccd4fca9b7
Bengisoo <zehrabengisu8@gmail.com>
2023-12-13 21:40:01 +0300

    [Update] AuthController

--- StudyTimer/Presentation/StudyTimer.MVC/Controllers/AuthController.cs
+++ StudyTimer/Presentation/StudyTimer.MVC/Controllers/AuthController.cs
@@ -1,29 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StudyTimer.Application.Models;
 using StudyTimer.Domain.Identity;
-using System.ComponentModel.DataAnnotations;
-using System.Reflection.Metadata.Ecma335;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using StudyTimer.MVC.Models;
+using NToastNotify;
 
 namespace StudyTimer.MVC.Controllers
 {
-    public class AuthController : ControllerBase
+    public class AuthController : Controller
     {
-
         private readonly UserManager<User> _userManager;
+
         private readonly SignInManager<User> _signInManager;
-        //private readonly IToastNotification _toastNotification;
+
+        private readonly IToastNotification _toastNotification;
         //private readonly IResend _resend;
         private readonly IWebHostEnvironment _environment;
 
-        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager, IWebHostEnvironment environment)
+        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager,
+            IWebHostEnvironment environment, IToastNotification toast)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _environment = environment;
+            _toastNotification = toast;
         }
 
         [HttpGet]
@@ -35,50 +35,91 @@ namespace StudyTimer.MVC.Controllers
             //}
 
             var registerViewModel = new AuthViewModel();
-            return View();
-            //return View(registerViewModel);
+
+
+            return View(registerViewModel);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> RegisterAsync(AuthRegisterViewModel registerViewModel)
+        [HttpGet]
+        public IActionResult Login()
         {
-            var userId = Guid.NewGuid();
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
 
+            var loginViewModel = new AuthLoginViewModel();
+
+            return View(loginViewModel);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(AuthLoginViewModel loginViewModel)
+        {
             if (!ModelState.IsValid)
-                return BadRequest(registerViewModel);
+                return View(loginViewModel);
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.Email);
+
+            if (user is null)
+            {
+                _toastNotification.AddErrorToastMessage("Your email or password is incorrect.");
+
+                return View(loginViewModel);
+            }
+
+            var loginResult = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, true, false);
+
+            if (!loginResult.Succeeded)
+            {
+                _toastNotification.AddErrorToastMessage("Your email or password is incorrect.");
+
+                return View(loginViewModel);
+            }
+
+            _toastNotification.AddSuccessToastMessage($"Welcome {user.UserName}!");
+
+            return RedirectToAction("Index", controllerName: "Home");
+        }
+    
+
+
+    [HttpPost]
+        public async Task<IActionResult> RegisterAsync(AuthViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(registerViewModel);
+
+            var userId = Guid.NewGuid();
 
             var user = new User()
             {
                 Id = userId,
                 Email = registerViewModel.Email,
                 FirstName = registerViewModel.FirstName,
-                LastName = registerViewModel.SurName,
+                LastName = registerViewModel.LastName,
                 Gender = registerViewModel.Gender,
                 BirthDate = registerViewModel.BirthDate.Value.ToUniversalTime(),
                 UserName = registerViewModel.UserName,
-                Sessions = registerViewModel.Sessions,
                 CreatedOn = DateTime.UtcNow,
                 CreatedByUserId = userId.ToString()
             };
 
-
-
             var identityResult = await _userManager.CreateAsync(user, registerViewModel.Password);
 
-            if (identityResult.Succeeded)
+            if (!identityResult.Succeeded)
             {
-                // User creation was successful.
-                // You might want to customize the response based on your requirements.
-                return Ok(new { Message = "User registration successful." });
-            }
-            else
-            {
-                // User creation failed. Return an appropriate response.
-                return BadRequest(new { Errors = identityResult.Errors });
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return View(registerViewModel);
             }
 
-
+           return RedirectToAction("Index", controllerName: "Home");
         }
-
     }
 }
+
