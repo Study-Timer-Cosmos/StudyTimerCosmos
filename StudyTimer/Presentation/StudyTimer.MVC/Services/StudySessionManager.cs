5fd6166a7709afbf995f0dba170546574fc108df
Bengisoo <zehrabengisu8@gmail.com>
2023-12-16 00:41:44 +0300

    [Update] StudySessionManager

--- StudyTimer/Presentation/StudyTimer.MVC/Services/StudySessionManager.cs
+++ StudyTimer/Presentation/StudyTimer.MVC/Services/StudySessionManager.cs
@@ -3,6 +3,7 @@ using StudyTimer.Domain.Entities;
 using StudyTimer.Domain.Identity;
 using StudyTimer.MVC.Models.Home;
 using StudyTimer.Persistence.Contexts;
+using System.Security.Claims;
 
 namespace StudyTimer.MVC.Services
 {
@@ -17,13 +18,14 @@ namespace StudyTimer.MVC.Services
             _userManager = userManager;
         }
 
-        public HomeCreateStudySessionResponseModel Create(HomeCreateStudySessionViewModel model, System.Security.Claims.ClaimsPrincipal user)
+        public HomeCreateStudySessionResponseModel Create(HomeCreateStudySessionViewModel model, ClaimsPrincipal user)
         {
+
             DateTimeOffset now = DateTimeOffset.UtcNow;
 
             Guid id = Guid.NewGuid();
             Guid dutyId = Guid.NewGuid();
-            _context.StudySessions.Add(new()
+            StudySession studySession = new()
             {
                 Id = id,
                 StartTime = now,
@@ -51,13 +53,110 @@ namespace StudyTimer.MVC.Services
                 },
                 CreatedByUserId = _userManager.GetUserId(user),
                 CreatedOn = DateTime.UtcNow
-            });
+            };
+            _context.StudySessions.Add(studySession);
+            var userId = _userManager.GetUserId(user);
+            User currentUser = _context.Users.FirstOrDefault(x => x.Id == Guid.Parse(userId));
+            if(currentUser.Sessions is null)
+            {
+            currentUser.Sessions = new List<UserStudySession>()
+                {
+                 new()
+                {
+                    UserId=Guid.Parse(userId),
+                    StudySessionId= studySession.Id,
+                    CreatedByUserId= userId,
+                    CreatedOn = DateTime.UtcNow,
+
+
+                }
+
+            };
+
+            }
+            else
+            {
+                currentUser.Sessions.Add(new()
+                {
+                    UserId = Guid.Parse(userId),
+                    StudySessionId = studySession.Id,
+                    CreatedByUserId = userId,
+                    CreatedOn = DateTime.UtcNow,
+
+
+                });
+            }
+            
+
+
+
             _context.SaveChanges();
 
             return new()
             {
                 Succeeded = true
             };
+        }
+
+        public HomeGetStatisticsViewModel Statistics(ClaimsPrincipal user)
+        {
+            var userId = _userManager.GetUserId(user);
+            //var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            User User = _context.Users.FirstOrDefault(x => x.Id == Guid.Parse(userId));
+
+            List<UserStudySession> sessions = _context.UserStudySessions
+                .Where(x => x.UserId == Guid.Parse(userId))
+                .ToList();
+
+            int totalSessions = sessions.Count();
+
+            TimeSpan totalTime = TimeSpan.Zero;
+
+            foreach (var session in sessions)
+            {
+                StudySession studySession = _context.StudySessions
+                    .FirstOrDefault(x => x.Id == session.StudySessionId);
+                TimeSpan time = (studySession.EndTime - studySession.StartTime);
+                totalTime += time;
+            }
+
+            List<Duty> allDuties = new List<Duty>();
+            int completedDutiesCount =0;
+            foreach (var userSession in sessions)
+            {
+                StudySession studySession = userSession.StudySession;
+                List<Duty> sessionDuties = _context.Duties.Where(x => x.SessionId == studySession.Id).ToList();
+                allDuties.AddRange(sessionDuties);
+            }
+            completedDutiesCount = allDuties.Where(x=>x.isFinished).Count();
+            int incompletedDutiesCount = allDuties.Count() - completedDutiesCount;
+
+
+            string mostRepeatedTopic = allDuties
+                .GroupBy(x => x.Topic.ToLower())
+                .OrderByDescending(x => x.Count())
+                .Select(x => x.Key)
+                .FirstOrDefault();
+
+
+            HomeGetStatisticsViewModel viewModel = new()
+            {
+                TotalStudySessions = totalSessions,
+                TotalStudyTime = totalTime,
+                MostStudiedTopic = mostRepeatedTopic,
+                TotalCompletedDuties = completedDutiesCount,
+                TotalIncompleteDuties = incompletedDutiesCount,
+                LastStudySessionDate = DateTime.Now,
+
+
+
+                //public string MostStudiedCategory { get; set; }
+                //public int TotalCompletedDuties { get; set; }
+                //public int TotalIncompleteDuties { get; set; }
+                //public DateTimeOffset LastStudySessionDate { get; set; }
+
+            };
+            return viewModel;
         }
     }
 }
