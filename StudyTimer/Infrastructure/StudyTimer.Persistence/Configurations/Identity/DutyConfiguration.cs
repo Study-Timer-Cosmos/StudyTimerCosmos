a2a61ee9aad312e87e1f7c87aac8676907ec1ea4
Bengisoo <zehrabengisu8@gmail.com>
2023-12-16 17:53:07 +0300

    [Update] Configurations

--- StudyTimer/Infrastructure/StudyTimer.Persistence/Configurations/Identity/DutyConfiguration.cs
+++ StudyTimer/Infrastructure/StudyTimer.Persistence/Configurations/Identity/DutyConfiguration.cs
@@ -23,6 +23,32 @@ namespace StudyTimer.Persistence.Configurations.Identity
 
             builder.Property(x => x.isFinished).IsRequired();
 
+
+            // Common Fields
+            // CreatedByUserId
+            builder.Property(x => x.CreatedByUserId).IsRequired();
+            builder.Property(x => x.CreatedByUserId).HasMaxLength(75);
+
+            // CreatedOn
+            builder.Property(x => x.CreatedOn).IsRequired();
+
+            // ModifiedByUserId
+            builder.Property(x => x.ModifiedByUserId).IsRequired(false);
+            builder.Property(x => x.ModifiedByUserId).HasMaxLength(75);
+
+            // LastModifiedOn
+            builder.Property(x => x.ModifiedOn).IsRequired(false);
+
+            // DeletedByUserId
+            builder.Property(x => x.DeletedByUserId).IsRequired(false);
+            builder.Property(x => x.DeletedByUserId).HasMaxLength(75);
+
+            // DeletedOn
+            builder.Property(x => x.DeletedOn).IsRequired(false);
+
+            // IsDeleted
+            builder.Property(x => x.IsDeleted).IsRequired();
+
             builder.ToTable("Duties");
         }
     }
